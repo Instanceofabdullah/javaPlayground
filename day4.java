ac7e2d10b009f70f18dceaee897ef0c92c3553de
instanceofabdullah <instanceofabdullah@gmail.com>
2026-03-30 10:47:46 +0530

    Day 4 - Arrays

---
+++ day4.java
@@ -0,0 +1,77 @@
+public class day4
+{
+
+
+    public static void main(String args[])
+    {
+        // System.out.println("Day 4 - Arrays!");
+
+        // // creating an array in java
+        // int arr[] = {1, 2, 3, 4};
+
+        // // printing each element with for loop
+        // for(int i = 0; i < 4; i++){
+        //     System.out.println(arr[i]);
+        // }
+
+
+        // Creating an empty array with given size and type
+        // int arr[] = new int[5];
+
+        // Another array is to be copied
+        // int arr2[] = {10, 45, 27, 69, 14};
+        
+        // // Assigning element to array using index
+        // arr[4] = 50;
+
+        // // Copying elements from arr2 to arr
+        // for(int i = 0; i < 5; i++){
+        //     arr[i] = arr2[i];
+        // }
+        // System.out.println(arr[7]);
+
+
+        // Multi-dimensional Arrays
+        // int arr2d[][] = new int[3][2];
+
+        // arr2d[2][1] = 10;
+
+        // System.out.println(arr2d[2][1]); 
+
+
+        // // Getting random values - (int)(Math.random() * 100)
+        // int arr[] = new int[10];
+
+        // // and assigning them to array
+        // for (int i = 0; i < 10; i++) {
+        //     arr[i] = (int)(Math.random() * 100);
+        //     System.out.println(i + ". " + arr[i]);
+        // }
+
+        // Enhanced for loop use for arrays
+        // int arr[] = {1, 2, 3, 4, 5};
+
+        // for(int n : arr){
+        //     System.out.println(n);
+        // }
+
+        // // jagged arrays - basically given out size of sub-arrays of multi dimensional array later
+        // int arr[][] = new int[3][];   // jagged
+
+        // arr[0] = new int[5];    // O O O O O 
+        // arr[2] = new int[8];    // O O O O O O O O 
+        // arr[1] = new int[3];    // O O O 
+
+        
+        // // Finding the length of array 
+        // int arr[] = {1, 2, 3, 4, 5};
+
+        // int size = arr.length;       // property to check size of array
+        // System.out.println(size);    // prints 5
+
+        System.out.println("Day 4 - Arrays, Random values, multi-dimensional arrays, jagged arrays, enhanced for loop, length of array");
+
+
+
+    }
+}
