12eddbe949fd43ebd85128c8ce7dc9ed457c09d5
instanceofabdullah <instanceofabdullah@gmail.com>
2026-04-06 22:35:06 +0530

    Day 11 - Types of Interfaces

---
+++ day11/Day11.java
@@ -0,0 +1,15 @@
+public class Day11 {
+    public static void main(String[] args) {
+        System.out.println("Day 11 - Types on interface, ");
+
+        // Types of interface
+        // 1. Normal   (Multiple methods)
+        // 2. Functional SAM(Single Abstract Method)
+        // 3. Marker  (No methods)
+
+
+        // Reason to study less : Illness
+
+        
+    }
+}
