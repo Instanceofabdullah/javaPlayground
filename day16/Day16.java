2b7006c181766608d67235a263962791b9b96a0e
instanceofabdullah <instanceofabdullah@gmail.com>
2026-04-11 23:34:17 +0530

    Day 16 - Intro to Maven

---
+++ day16/Day16.java
@@ -0,0 +1,29 @@
+public class Day16
+{
+
+    public static void main()
+    {
+        System.out.println("Day 16 - Intro to Maven.. ");
+
+        // Java Code Flow
+        // Source code - .java --> Compile
+        // Byte Code - .class --> Test case
+        // Unit test - JUnit5  --> Package
+        // Jar/War Build --> Jar or War
+
+        // Maven helps in : 
+        // Java Build and project management tool
+        // Manages dependencies and automates the build process
+
+        // Maven is a build tool for Java projects which was developed by Apache Software Foundation
+
+        // Basically, It is used to automate the build process of java projects, including tasks such as compiling code, managing dependencies, packaging, testing and deployment.
+
+        // What Maven do? 
+        // Create the standard project folder structure
+        // Download and manage required dependencies/libraries automatically
+        // Execute unit test cases using testing framework like JUnit
+        // Compile the project's source code
+        // Package the project into a deployabke format such as JAR or WAR
+    }
+}
