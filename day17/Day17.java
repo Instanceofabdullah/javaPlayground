09f22da698961617cc7e7465ab340eea527f778d
instanceofabdullah <instanceofabdullah@gmail.com>
2026-04-12 23:50:39 +0530

    Day 17 - Instal Maven & Folder Structure

---
+++ day17/Day17.java
@@ -0,0 +1,17 @@
+public class Day17
+{
+
+    public static void main()
+    {
+        System.out.println("Day 17 - Installation & Maven Project Structure.. ");
+
+        // Standard Maven Project Structure
+        // src/main/java - Contains the main source code of the application.
+        // src/test/java - Contains the test source code for unit testing.
+        // src/main/resources - Contains non-code resources such as configuration files, images, etc.
+        // src/test/resources - Contains resources needed for testing.
+        // pom.xml - The Project Object Model file that defines the project configuration, dependencies, build settings, and plugins.
+
+        // The standard Maven project structure promotes consistency and organization across Java projects, making it easier for developers to navigate and manage their codebase effectively.
+    }
+}
