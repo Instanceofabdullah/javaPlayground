c1fed34a5259116cbbda4d0efdd576c0e4d76cf4
instanceofabdullah <instanceofabdullah@gmail.com>
2026-04-14 23:53:32 +0530

    Day 19 - pom file & mvm cmd

---
+++ day19/Day19.java
@@ -0,0 +1,20 @@
+public class Day19
+{
+    public static void main(String args[])
+    {
+        System.out.println("Day 19 - pom.xml & Maven Goals");
+
+
+        // pom.xml - Project Object Model
+        //      - XML file that contains information about the project and configuration details used by Maven to build the project
+
+        // Maven Goals - mvn [goal]
+        //      - A goal represents a specific task that contributes to the building and managing of a project
+        //      - Examples of goals include:
+        //          - mvn compile - compiles the source code
+        //          - mvn test - runs the tests
+        //          - mvn package - packages the compiled code
+        //          - mvn install - installs the package in the local repository
+
+    }
+}
